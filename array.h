10cbd0320c5a737bb8d73af395a2b24f9a5697bf
rucha2398 <36318998+rucha2398@users.noreply.github.com>
2020-01-24 18:16:26 -0500

    array.h

---
+++ array.h
@@ -0,0 +1,73 @@
+#pragma once
+
+#include "object.h"
+#include <assert.h>
+
+class Array : public Object {
+    public:
+        Object** arr_; // elements not owned
+        size_t size_; // number of elements in the list
+        size_t cap_; // capacity of the list
+
+        // constructs an empty list
+        Array() : Object() {
+            cap_ = 4;
+            size_ = 0;
+            arr_ = new Object*[cap_];
+        }
+
+        ~Array() {
+            delete[] arr_;
+        }
+
+        // returns the number of elements in this array
+        virtual size_t size();
+
+        // returns the index of the given object in this array
+        // returns an index greater than this array's size if there is no such element
+        virtual size_t index_of(Object* o);
+
+        // doubles the allocated space for this array
+        // @post maintains element indexing, capacity doubles
+        virtual void grow_();
+
+        // pushes the given object onto the end of this array
+        // @pre the given object is not null
+        // @post size = size + 1
+        // @invariant no elements are null
+        virtual void push(Object* o);
+        
+        // add the given element to the array at the given index
+        // @pre the given object is not null
+        // @post size = size + 1
+        // @invariant no elements are null
+        virtual void add(size_t idx, Object* o);
+        
+        //adds all given elements to the array at the given index
+        //@pre the given objects are not null
+        //@post size = size + a -> size_
+        //@invariant no elements are null
+        virtual void add_all(size_t idx, Array* a);
+        
+        //returns the element of the array at the given index
+        //@pre the index is in bounds
+        virtual Object* get(size_t idx);
+
+        //sets the given index to the given element
+        //@returns the overwritten object
+        virtual Object* set(size_t idx, Object* o);
+
+        //removes the object at the given index
+        //@pre the index is in bounds
+        //@return the overwritten object
+        virtual Object* remove(size_t idx);
+
+        //removes all elements from the array
+        virtual void clear();
+
+        //checks if this array is equal to the given object
+        virtual bool equals(Object* o);
+
+        //adds together the hashes of all of the objects
+        virtual size_t hash();
+};
